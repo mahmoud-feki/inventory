494b54090970806c6e6896d7a6324160e9639794
Mahmoud <feki.mahmoud00@gmail.com>
2019-03-27 16:58:31 +0100

    Modification 005

--- src/main/java/com/sqli/inventory/inventory/InventoryApplication.java
+++ src/main/java/com/sqli/inventory/inventory/InventoryApplication.java
@@ -35,7 +35,7 @@ public class InventoryApplication {
 
         //    d1.setProfil(profil);
         Data d2 = new Data("002", 15);
-        Data d3 = new Data("004", 15);
+        Data d3 = new Data("005", 15);
         datas.add(d1);
         datas.add(d2);
         datas.add(d3);
